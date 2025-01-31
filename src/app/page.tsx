eff386f1c3ed2743130067ea81d5bcd933714f77
710minjoon <annawa6@naver.com>
2025-01-31 13:48:09 +0900

    ✨feat:Routing테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
-import MainPage from "@/components/main-page";
+import Link from "next/link";
 
 export default function Home() {
-  return <MainPage></MainPage>;
+  return (
+    <div>
+      <Link href="/main">
+        <button>메인페이지로 이동</button>
+      </Link>
+    </div>
+  );
 }
