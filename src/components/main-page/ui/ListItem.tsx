21657351bbf1e2f336a71460f3e304ff1fcf60eb
710minjoon <annawa6@naver.com>
2025-02-25 22:57:17 +0900

    ✨feat:listData파싱

--- src/components/main-page/ui/ListItem.tsx
+++ src/components/main-page/ui/ListItem.tsx
@@ -1,12 +1,21 @@
 import { GitHubRelease } from "@/shared/types/githubReleaseType";
 import { List } from "./indexCss";
+import ReactMarkdown from "react-markdown";
 
 interface ListItemProps {
   listData: GitHubRelease;
 }
 
 const ListItem = ({ listData }: ListItemProps) => {
-  return <List>{listData.created_at}</List>;
+  return (
+    <List>
+      <h1>{listData.name}</h1>
+      <p>{listData.created_at}</p>
+      <div>
+        <ReactMarkdown>{listData.body}</ReactMarkdown>
+      </div>
+    </List>
+  );
 };
 
 export default ListItem;
--- src/components/main-page/ui/indexCss.ts
+++ src/components/main-page/ui/indexCss.ts
@@ -2,4 +2,20 @@ import styled from "styled-components";
 
 export const List = styled.li`
   border: 1px solid black;
+  padding: 2rem;
+  margin-bottom: 5px;
+
+  & > h1 {
+    font-size: 20px;
+  }
+
+  & > p:nth-of-type(1) {
+    padding: 10px 0 20px 0;
+  }
+
+  & > div {
+    font-size: 14px;
+    color: rgba(0, 0, 0, 0.8);
+    line-height: 20px;
+  }
 `;
